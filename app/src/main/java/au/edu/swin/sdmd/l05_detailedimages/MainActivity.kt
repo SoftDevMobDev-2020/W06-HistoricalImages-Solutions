079bb0e0fef5cb9b28cfb385a4378e6fd581ddd9
N. A. Ronald <nronald@swin.edu.au>
2020-07-09 17:03:22 +1000

    First commit -- no intents.

---
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
@@ -0,0 +1,22 @@
+package au.edu.swin.sdmd.l05_detailedimages
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val station = Location("Glenferrie Station", "Ada May Plante",
+                -37.821539f, 145.036473f)
+
+        val vStation = findViewById<TextView>(R.id.station)
+        vStation.setOnClickListener {
+
+        }
+    }
+
+
+}
