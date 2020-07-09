d9f0127897c86c5b9a1ac217afa6d13ffa44829f
N. A. Ronald <nronald@swin.edu.au>
2020-07-09 17:35:34 +1000

    Added intents and parcelable.

--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
@@ -1,5 +1,6 @@
 package au.edu.swin.sdmd.l05_detailedimages
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         val vStation = findViewById<TextView>(R.id.station)
         vStation.setOnClickListener {
-
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("location", station)
+            startActivity(intent)
         }
     }
 
