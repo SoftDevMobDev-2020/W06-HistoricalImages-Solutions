f28769eaf89e88bff87793ef8a253f0ce2a7e31d
N. A. Ronald <nronald@swin.edu.au>
2020-09-11 17:20:18 +1000

    Added in "saving" between activities.

--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/DetailActivity.kt
@@ -1,15 +1,19 @@
 package au.edu.swin.sdmd.l05_detailedimages
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
+    lateinit var location: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val location = intent.getParcelableExtra<Location>("location")
+        location = intent.getParcelableExtra<Location>("location")
 
         val vName = findViewById<TextView>(R.id.name)
         vName.text = location.name
@@ -17,5 +21,17 @@ class DetailActivity : AppCompatActivity() {
         val vAuthor = findViewById<TextView>(R.id.author)
         vAuthor.text = location.author
 
+        val vVisited = findViewById<Switch>(R.id.visited)
+        vVisited.isChecked = location.visited
+
+    }
+
+    override fun onBackPressed() {
+        location.visited = findViewById<Switch>(R.id.visited).isChecked
+        val i = intent.apply {
+            putExtra("visited", location)
+        }
+        setResult(Activity.RESULT_OK, i)
+        super.onBackPressed()
     }
 }
--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
@@ -1,23 +1,42 @@
 package au.edu.swin.sdmd.l05_detailedimages
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var station: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val station = Location("Glenferrie Station", "Ada May Plante",
+        station = Location("Glenferrie Station", "Ada May Plante",
                 -37.821539f, 145.036473f)
 
         val vStation = findViewById<TextView>(R.id.station)
         vStation.setOnClickListener {
             val intent = Intent(this, DetailActivity::class.java)
             intent.putExtra("location", station)
-            startActivity(intent)
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) return
+
+        if (requestCode == 0) {
+            val location = data?.getParcelableExtra<Location>("visited")
+            val vStation = findViewById<TextView>(R.id.station)
+            location?.let {
+                vStation.setTextColor(if (location.visited) Color.GREEN else Color.RED)
+                station.visited = location.visited
+            }
         }
     }
 
